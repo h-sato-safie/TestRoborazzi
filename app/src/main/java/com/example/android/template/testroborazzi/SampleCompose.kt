f7285497676cfeece7a3ee8a432b4f595e252c1b
JoesPredator <sato.args@gmail.com>
2023-12-07 20:21:57 +0900

    lib update

---
+++ app/src/main/java/com/example/android/template/testroborazzi/SampleCompose.kt
@@ -0,0 +1,30 @@
+package com.example.android.template.testroborazzi
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SampleText() {
+    val size = 40.sp
+    val spaceHeight = 60.dp
+    Column {
+        Spacer(modifier = Modifier.height(spaceHeight))
+        Text(text = "Hallo World!", fontSize = size)
+        Spacer(modifier = Modifier.height(spaceHeight))
+        Text(text = "テストテストあいうえお１２３４５６７", fontSize = size)
+        Spacer(modifier = Modifier.height(spaceHeight))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SampleTextPreview() {
+    SampleText()
+}
